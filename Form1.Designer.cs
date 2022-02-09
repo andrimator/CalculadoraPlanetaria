0f3ba0c755fafc3a5faa29e3035a18773e822991
André A <andrimator@gmail.com>
2022-02-08 22:56:58 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace CalculadoraPlanetaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxPlaneta = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxTuPeso = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbxPlaneta
+            // 
+            this.cbxPlaneta.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxPlaneta.FormattingEnabled = true;
+            this.cbxPlaneta.Items.AddRange(new object[] {
+            "Mercurio",
+            "Venus",
+            "Tierra",
+            "Luna",
+            "Marte",
+            "Jupiter",
+            "Saturno",
+            "Urano",
+            "Neptuno"});
+            this.cbxPlaneta.Location = new System.Drawing.Point(132, 12);
+            this.cbxPlaneta.Name = "cbxPlaneta";
+            this.cbxPlaneta.Size = new System.Drawing.Size(121, 21);
+            this.cbxPlaneta.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selecciona tu planeta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "¿Cuánto pesas en la Tierra? (kg)";
+            // 
+            // tbxTuPeso
+            // 
+            this.tbxTuPeso.Location = new System.Drawing.Point(180, 43);
+            this.tbxTuPeso.Name = "tbxTuPeso";
+            this.tbxTuPeso.Size = new System.Drawing.Size(73, 20);
+            this.tbxTuPeso.TabIndex = 3;
+            this.tbxTuPeso.TextChanged += new System.EventHandler(this.tbxTuPeso_TextChanged);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(113, 74);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 109);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbxTuPeso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxPlaneta);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora Planetaria";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxPlaneta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxTuPeso;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraPlanetaria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cbxPlaneta.SelectedIndex = 2;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if(tbxTuPeso.TextLength != 0)
+            {
+                try
+                {
+                    double PesoPlaneta = Math.Round(CalcularPesoPlaneta(Convert.ToDouble(tbxTuPeso.Text), cbxPlaneta), 2);
+                    string InfoPesoPlaneta = "En " + cbxPlaneta.SelectedItem.ToString() + ", pesarías: " + PesoPlaneta + "kg.";
+                    MessageBox.Show(InfoPesoPlaneta, cbxPlaneta.SelectedItem.ToString(), MessageBoxButtons.OK);
+                }
+                catch (SystemException exception)
+                {
+                    MessageBox.Show(exception.ToString(), "La calculadora solo funciona con numeros!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else MessageBox.Show("No has ingresado tu peso!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+        }
+
+        private double CalcularPesoPlaneta(double peso, ComboBox selection)
+        {
+            peso = peso / 9.81f;
+            switch (selection.SelectedItem)
+            {
+                case "Mercurio":
+                    return peso * 3.7f;
+                case "Venus":
+                    return peso * 8.87f;
+                case "Tierra":
+                    return peso * 9.81f;
+                case "Luna":
+                    return peso * 1.62f;
+                case "Marte":
+                    return peso * 3.721f;
+                case "Jupiter":
+                    return peso * 24.79f;
+                case "Saturno":
+                    return peso * 10.44f;
+                case "Urano":
+                    return peso * 8.87f;
+                case "Neptuno":
+                    return peso * 11.15f;
+                default:
+                    break;
+            }
+            return 0;
+        }
+
+        private void tbxTuPeso_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
